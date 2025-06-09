7384383e4c7c50ff1d242e851fe75538798a2b0b
AngryNxho <pleasetrixcl@gmail.com>
2025-06-09 17:08:55 -0400

    s2 listo

--- Sumativa1/src/app/home/home.module.ts
+++ Sumativa1/src/app/home/home.module.ts
@@ -1,19 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule, DateAdapter, NativeDateAdapter } from '@angular/material/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HomePageRoutingModule } from './home-routing.module';
-
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    ReactiveFormsModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [
+    { provide: DateAdapter, useClass: NativeDateAdapter }
+  ]
 })
 export class HomePageModule {}
--- Sumativa1/src/app/home/home.page.ts
+++ Sumativa1/src/app/home/home.page.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
   standalone: false,
-})
-export class HomePage {
 
-  constructor() {}
+})
+export class HomePage implements OnInit {
+  usuario: string = '';
+  nombre: string = '';
+  apellido: string = '';
+  nivelEducacion: string = '';
+  fechaNacimiento: Date | null = null;
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit() {
+    const navigation = this.router.getCurrentNavigation();
+    if (navigation?.extras?.state) {
+      this.usuario = navigation.extras.state['usuario'];
+    }
+  }
+
+  mostrar() {
+    alert(`Nombre: ${this.nombre}\nApellido: ${this.apellido}`);
+  }
+
+  limpiar() {
+  this.nombre = '';
+  this.apellido = '';
+  this.nivelEducacion = '';
+  this.fechaNacimiento = undefined as any;
+
+  const inputs = document.querySelectorAll('.animate-input');
+  inputs.forEach(input => {
+    const element = input as HTMLElement;
+    element.classList.remove('slide');
+    void element.offsetWidth;
+    element.classList.add('slide');
+  });
+}
 
 }
