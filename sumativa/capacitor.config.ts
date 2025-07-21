b98a0bc62fb043b6e5a9ba38e21e54534ac2d7ec
AngryNxho <pleasetrixcl@gmail.com>
2025-07-20 23:23:43 -0400

    agregar nuevo proyecto

--- sumativa/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'sumativa',
-  webDir: 'www'
-};
-
-export default config;
